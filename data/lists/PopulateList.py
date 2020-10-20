ebeb615a3b53847f3a5d1da9a3e163fa3e31a866
4monkw84 <4monkw84@solent.ac.uk>
2020-10-20 11:26:15 +0100

    Added comments

--- data/lists/PopulateList.py
+++ data/lists/PopulateList.py
@@ -4,18 +4,26 @@ def directions():
 
 def menu():
 	print("Please select a direction:")
+	#store directions as local var
 	direction = directions()
+	#iterate through directions
 	for x in range(len(direction)):
+		#print directions and indicies
 		print("{index}: {facing}".format(index = x, facing = direction[x]))
+	#get user input
 	usrInput = input()
+	#display user choice
 	print("{index}: {facing}".format(index = usrInput, facing = direction[int(usrInput)]))
+	#return user choice
 	return direction[int(usrInput)]
 
 def run():
 	route = []
 	print("Working out an escape route...")
 	for x in range(5):
+		#get and store user input into route list 5 times
 		route.append(menu())
-	print(route)
+	#print list contents
+	print("Escape route: ", route)
 
 run()
